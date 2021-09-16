17fcb204b5c814e409c8b150838bf5428673f1f9
Umme Kawser Sinthia <40598386+sinthy08@users.noreply.github.com>
2021-09-16 16:52:22 +0600

    Chenged the picture of home page and icon to BS and removed login

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>DS|Show Task</title>
+    <title>DS | Show Task</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
